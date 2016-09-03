3aac29d0f9cb65ba5f0d687e5ccfe1eb0f2cec5a
NicolasAtYouyi <nicolas@youyiii.com>
2016-09-03 13:53:32 +0800

    new project
    create a project

---
+++ BenzGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BenzGame
+//
+//  Created by Nicolas on 16/9/3.
+//  Copyright © 2016年 AndyVan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BenzGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BenzGame
+//
+//  Created by Nicolas on 16/9/3.
+//  Copyright © 2016年 AndyVan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
